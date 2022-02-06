4630cf59c3218fd0f20e6e0ca1ca4d5360e79b82
Omega579 <88238063+iDevYT@users.noreply.github.com>
2022-02-06 18:07:26 +0100

    made HTML

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome To iDev!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/channel/UCwYJtY18T2n7fysL7qJeB2g"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          come
         </a>
       </header>
     </div>
